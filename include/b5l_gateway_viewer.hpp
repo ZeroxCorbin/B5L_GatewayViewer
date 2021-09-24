636eec701cd2c17ea6e54cff585bb3d4022ee658
Jack <jack@jackb.me>
2021-09-23 22:48:43 -0400

    adding filters

--- include/b5l_gateway_viewer.hpp
+++ include/b5l_gateway_viewer.hpp
@@ -17,9 +17,15 @@ using namespace std::chrono_literals;
 #include <pcl/filters/passthrough.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/bilateral.h>
+#include <pcl/features/normal_3d_omp.h>
+
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
 
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/io/io.h>
 #include <pcl/io/pcd_io.h>
 
 #include "clsTCPSocket.hpp"
+#include "clsTimer.hpp"
